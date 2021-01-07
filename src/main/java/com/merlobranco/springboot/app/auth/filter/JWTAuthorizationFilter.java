86d44eae93aba14eb07cb16db44d4422aca5f578
Brais Cidras <braisomary@gmail.com>
2021-01-07 19:22:15 +0200

    JWTAuthorizationFilter. Checking if the request header has a JWT

---
+++ src/main/java/com/merlobranco/springboot/app/auth/filter/JWTAuthorizationFilter.java
@@ -0,0 +1,33 @@
+package com.merlobranco.springboot.app.auth.filter;
+
+import java.io.IOException;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+
+public class JWTAuthorizationFilter extends BasicAuthenticationFilter {
+
+	public JWTAuthorizationFilter(AuthenticationManager authenticationManager) {
+		super(authenticationManager);
+	}
+
+	@Override
+	protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain chain)
+			throws IOException, ServletException {
+		
+		String header = request.getHeader("Authorization");
+		if (!requiresAuthentication(header)) {
+			chain.doFilter(request, response);
+			return;
+		}
+	}
+	
+	protected boolean requiresAuthentication(String header) {
+		return header != null && header.startsWith("Bearer ");
+	}
+}
